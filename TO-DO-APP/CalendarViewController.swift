de873d8c3058807d7c1698f8e1535151a5b7a543
dieptran00888 <dieptx.ptit@gmail.com>
2017-08-04 10:47:39 +0700

    UI calendar screen

---
+++ TO-DO-APP/CalendarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CalendarViewController.swift
+//  TO-DO-APP
+//
+//  Created by tran.xuan.diep on 8/4/17.
+//  Copyright © 2017 framgia. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+//    @IBOutlet weak var delegate: FSCalendar!
+//    @IBOutlet weak var dataSource: FSCalendar!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
